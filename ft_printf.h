cb36015ed2b42996ffce991ed2383d96ead0b1f8
IIAydedeII <ahmetyasindd@gmail.com>
2026-02-13 09:52:39 +0300

    Justified includes by using comments

--- ft_printf.h
+++ ft_printf.h
@@ -6,15 +6,15 @@
 /*   By: adede <adede@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/06 13:34:11 by adede             #+#    #+#             */
-/*   Updated: 2026/02/10 14:59:14 by adede            ###   ########.fr       */
+/*   Updated: 2026/02/12 17:08:15 by adede            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 # include "Libft/libft.h"
-# include <stdarg.h>
-# include <stdint.h>
+# include <stdarg.h> // for Variadic Functions
+# include <stdint.h> // for storing pointer as an int (uintptr_t)
 
 int	ft_printf(const char *format, ...);
 int	ft_print_char(char c);
