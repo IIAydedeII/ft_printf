0e40004560a2c689d3a86fe455023c634954a8eb
IIAydedeII <ahmetyasindd@gmail.com>
2026-02-23 10:36:32 +0300

    Implemented precision zeros

--- src_bonus/ft_padding.c
+++ src_bonus/ft_padding.c
@@ -6,7 +6,7 @@
 /*   By: adede <adede@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 12:42:29 by adede             #+#    #+#             */
-/*   Updated: 2026/02/23 09:49:04 by adede            ###   ########.fr       */
+/*   Updated: 2026/02/23 10:35:07 by adede            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,24 @@
 
 void	ft_padding(int length, t_options *options)
 {
-	int	padding;
+	int		padding_zero;
+	int		padding;
+	bool	numeric;
 
-	padding = options->width - length;
+	padding_zero = options->precision - length;
+	if (padding_zero < 0)
+		padding_zero = 0;
+	padding = options->width - length - padding_zero;
 	if (padding < 0)
 		padding = 0;
+	numeric = ft_strchr("diuxX", options->specifier);
 	while (padding--)
 	{
-		if (options->flags.zero && ft_strchr("diuoxX", options->specifier))
+		if (options->flags.zero && numeric && options->precision == 0)
 			ft_putchar('0', options);
 		else
 			ft_putchar(' ', options);
 	}
+	while (padding_zero-- && numeric)
+		ft_putchar('0', options);
 }
